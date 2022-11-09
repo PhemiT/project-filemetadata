96a4a807fafb7890daf43f523fc6e96d59494d8b
Olufemi Olubukunmi <phemiwrites@gmail.com>
2022-11-09 08:03:15 +0100

    endpoint

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+// require multer
+const multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +15,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  try {
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+  } catch (err) {
+    res.send(400);
+  }
+});
 
 
 const port = process.env.PORT || 3000;
